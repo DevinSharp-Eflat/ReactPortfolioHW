0a97dac533a7e662010ad647c0126a31483469d5
Devin Sharp <sharpdevin467@gmail.com>
2024-11-05 23:56:42 -0800

    fixed button text colors

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -6,10 +6,10 @@ export default function Footer() {
         <>
         <Stack direction={"row"} spacing={20} sx={{alignContent: 'center'}}>
             <ButtonGroup size="medium">
-            <Button variant="contained" sx={{backgroundColor: "#a2c7e5"}}
+            <Button variant="contained" sx={{backgroundColor: "#a2c7e5", color: "#303A2B"}}
                 onClick={() => { window.location.href = "https://github.com/DevinSharp-Eflat";
                 }}>Github</Button>
-            <Button variant="contained" sx={{backgroundColor: "#a2c7e5"}}
+            <Button variant="contained" sx={{backgroundColor: "#a2c7e5", color: "#303A2B"}}
                 onClick={() => { window.location.href = "https://www.linkedin.com/in/devinisharp/";
                 }}>LinkedIn</Button>
             </ButtonGroup>
--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -7,19 +7,19 @@ export default function Header() {
         <Stack direction={"row"} spacing={20} sx={{alignContent: 'center'}}>
         <h1>Devin S.</h1>
         <ButtonGroup size="large" variant="contained" aria-label="website nav" sx={{justifyContent:  'right'}}>
-            <Button variant="contained" sx={{backgroundColor: "#a2c7e5"}}
+            <Button variant="contained" sx={{backgroundColor: "#a2c7e5", color: "#303A2B"}}
                 onClick={() => { let path = '/';
                 window.location.href = path;
                 }}>Home</Button>
-            <Button variant="contained" sx={{backgroundColor: "#a2c7e5"}}
+            <Button variant="contained" sx={{backgroundColor: "#a2c7e5", color: "#303A2B"}}
                 onClick={() => { let path = '/portfolio';
                 window.location.href = path;
                 }}>Portfolio</Button>
-            <Button variant="contained" sx={{backgroundColor: "#a2c7e5"}}
+            <Button variant="contained" sx={{backgroundColor: "#a2c7e5", color: "#303A2B"}}
                 onClick={() => { let path = '/contact';
                 window.location.href = path;
                 }}>Contact</Button>
-            <Button variant="contained" sx={{backgroundColor: "#a2c7e5"}}
+            <Button variant="contained" sx={{backgroundColor: "#a2c7e5", color: "#303A2B"}}
                 onClick={() => { let path = '/resume';
                 window.location.href = path;
                 }}>Resume</Button>

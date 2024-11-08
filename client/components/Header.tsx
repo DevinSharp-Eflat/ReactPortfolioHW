4e9a5a882d411852ac4d46d72e50d3a930790596
Devin Sharp <sharpdevin467@gmail.com>
2024-11-07 20:44:46 -0800

    fix header nav

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -9,19 +9,19 @@ export default function Header() {
         <h1>Devin S.</h1>
         <ButtonGroup size="large" variant="contained" aria-label="website nav" sx={{justifyContent:  'right'}}>
             <Button variant="contained" sx={{backgroundColor: "#a2c7e5", color: "#303A2B"}}
-                onClick={() => { let path = '/';
+                onClick={() => { let path = './';
                 window.location.href = path;
                 }}>Home</Button>
             <Button variant="contained" sx={{backgroundColor: "#a2c7e5", color: "#303A2B"}}
-                onClick={() => { let path = '/portfolio';
+                onClick={() => { let path = './portfolio';
                 window.location.href = path;
                 }}>Portfolio</Button>
             <Button variant="contained" sx={{backgroundColor: "#a2c7e5", color: "#303A2B"}}
                 onClick={() => { 
-                    window.location.href = "/contact";
+                    window.location.href = "./contact";
                 }}>Contact</Button>
             <Button variant="contained" sx={{backgroundColor: "#a2c7e5", color: "#303A2B"}}
-                onClick={() => { let path = '/resume';
+                onClick={() => { let path = './resume';
                 window.location.href = path;
                 }}>Resume</Button>
         </ButtonGroup>
